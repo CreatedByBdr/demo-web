40245021ec4e483f49bc7abb33ee2609e0bd72d4
bgharbi <bgharbi-ext@globalpos.fr>
2023-03-15 13:25:00 +0100

    test correction

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 
-var api_url = process.env.API_HOST + '/api/status';
+var api_url = 'https://' + process.env.API_HOST + '/api/status';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
